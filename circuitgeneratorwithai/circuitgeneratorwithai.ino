ca01161d2ec3d2825892f633577c86987b852e49
agnitduke <154935676+agnitduke@users.noreply.github.com>
2023-12-26 19:03:45 -0800

    Add files via upload

--- circuitgeneratorwithai/circuitgeneratorwithai.ino
+++ circuitgeneratorwithai/circuitgeneratorwithai.ino
@@ -1,94 +1,96 @@
+ 
 #include <Arduino.h>
 #include <EEPROM.h>
-
+ 
 #define GRID_SIZE 32
 #define EEPROM_ADDRESS 0
-
+ 
 enum ComponentType {
   // ... (existing component types)
   NEW_COMPONENT_TYPE_1,
   NEW_COMPONENT_TYPE_2,
   // Define new component types
 };
-
+ 
 const char* componentNames[] = {
   // ... (existing component names)
   "New Component 1",
   "New Component 2",
   // Define names for new component types
 };
-
+ 
 struct CircuitComponent {
   ComponentType type;
 };
-
+ 
 CircuitComponent circuitGrid[GRID_SIZE][GRID_SIZE];
-
+ 
 void initializeGrid() {
   // Implementation of initializeGrid()
 }
-
+ 
 void printGrid() {
   // Implementation of printGrid()
 }
-
+ 
 void addComponent(int row, int col, ComponentType type) {
   // Implementation of addComponent()
 }
-
+ 
 void subtractComponent(int row, int col) {
   // Implementation of subtractComponent()
 }
-
+ 
 void generateRandomDiagram() {
   // Implementation of generateRandomDiagram()
 }
-
+ 
 bool testCircuit() {
   // Implementation of testCircuit()
 }
-
+ 
 void saveFeedback(bool isPositive) {
   // Implementation of saveFeedback()
 }
-
+ 
 bool getLastFeedback() {
   // Implementation of getLastFeedback()
 }
-
+ 
 void generateCorrection() {
   // Implementation of generateCorrection()
 }
-
+ 
 bool isValidCoordinate(int row, int col) {
   // Implementation of isValidCoordinate()
 }
-
+ 
 void setup() {
   Serial.begin(9600);
   initializeGrid();
   Serial.println("Type 'generate' to create a random diagram.");
 }
-
+ 
 void loop() {
   if (Serial.available() > 0) {
     String userInput = Serial.readStringUntil('\n');
     userInput.trim();
-
+ 
     // Handle user input
     // (remaining code remains the same as provided)
   }
 }
-
+ 
 void saveToEEPROM(int address, const uint8_t* data, int dataSize) {
   for (int i = 0; i < dataSize; ++i) {
     EEPROM.write(address + i, data[i]);
     delay(4); // Delay to ensure data is written properly to EEPROM
   }
 }
-
+ 
 void readFromEEPROM(int address, uint8_t* data, int dataSize) {
   for (int i = 0; i < dataSize; ++i) {
     data[i] = EEPROM.read(address + i);
   }
 }
+ 
